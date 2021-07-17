b903ad8c4955c4159bc112188e3ca73e0c39bdaf
cfloryiv <cfloryiv@rcn.com>
2021-07-16 20:38:13 -0400

    added doctor name to appts get

--- controllers/apptsController.js
+++ controllers/apptsController.js
@@ -1,25 +1,49 @@
-function apptsController(Appt) {
+function apptsController(Appt, Address) {
   function post(req, res) {
     const appt = new Appt(req.body);
     appt.save();
     res.status(201);
     return res.json(appt);
   }
-  function get(req, res) {
+  async function buildAppts(query) {
+    const doc=await Appt.find(query).sort('date').exec();
+    const promises=[];
+    await doc.forEach(({ _doc }) => {
+      (async () => {
+        await promises.push(
+          new Promise((resolve) => {
+            (async () => {
+              const appt=_doc;
+              const { empid } = appt;
+              let doctorName;
+              try {
+                const doctor=await Address.findOne({ userid: empid });
+                const { name } = doctor;
+                doctorName=name;
+
+              }
+              catch {
+                doctorName=empid;
+              }
+              const newAppt = {doctorName, ...appt };
+              resolve(newAppt);
+            })();
+          })
+        );
+      })();
+    });
+    return Promise.all(promises)
+      .then((res) => {
+        return res;
+      });
+  }
+  async function get(req, res) {
     const query = {};
     if (req.query.date) {
       query.date = req.query.date;
     }
-    Appt.find(query, (err, appts) => {
-      if (err) {
-        return res.send(err);
-      }
-      const returnAppts = appts.map((appt) => {
-        const newAppt = appt.toJSON();
-        return newAppt;
-      });
-      return res.json(returnAppts);
-    });
+    const result = await buildAppts(query);
+    return res.json(result);
   }
   return { post, get };
 }
